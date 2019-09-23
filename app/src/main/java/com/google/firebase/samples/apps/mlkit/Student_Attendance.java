3bfc6f7029467ff9aea5d2b4acf6c9f2d6f2177e
yashk04 <yashkasat04@gmail.com>
2019-09-23 21:35:36 +0530

    Student UI

---
+++ app/src/main/java/com/google/firebase/samples/apps/mlkit/Student_Attendance.java
@@ -0,0 +1,13 @@
+package com.google.firebase.samples.apps.mlkit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Student_Attendance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student__attendance);
+    }
+}
