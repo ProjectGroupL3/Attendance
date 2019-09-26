9eb9d4d70f4db78d6e1ea1e7deb1667988a2f69f
Ashawe <harshsaglani@gmail.com>
2019-09-26 11:41:08 +0530

    Add Defaulters list UI

--- app/src/main/java/com/google/firebase/samples/apps/mlkit/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/google/firebase/samples/apps/mlkit/ui/gallery/GalleryFragment.java
@@ -1,9 +1,15 @@
 package com.google.firebase.samples.apps.mlkit.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,11 +18,17 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+import com.google.firebase.samples.apps.mlkit.LivePreviewActivity;
 import com.google.firebase.samples.apps.mlkit.R;
 
 public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
+    private Spinner spinner;
+    private Button mBtnAttendance;
+    private ChipGroup chipGroup;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +42,26 @@ public class GalleryFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+
+        spinner = root.findViewById(R.id.spinner_list);
+        mBtnAttendance = root.findViewById(R.id.btn_take_attendance);
+        chipGroup = root.findViewById(R.id.cg_stud_names);
+
+        ArrayAdapter<String> spinner_adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_dropdown_item, galleryViewModel.getListOfSubjects());
+        spinner.setAdapter(spinner_adapter);
+
+        mBtnAttendance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chipGroup.removeAllViews();
+                Chip chip = new Chip(getContext());Chip chip1 = new Chip(getContext());Chip chip2 = new Chip(getContext());Chip chip3 = new Chip(getContext());
+                chip.setText("Kunal Kamra");chip1.setText("John Doe");chip2.setText("Lorem ipsum");chip3.setText("Tanmay Bhatt");
+                chipGroup.addView(chip);chipGroup.addView(chip1);
+                chipGroup.addView(chip2);chipGroup.addView(chip3);
+            }
+        });
+
         return root;
     }
 }
--- app/src/main/java/com/google/firebase/samples/apps/mlkit/ui/gallery/GalleryViewModel.java
+++ app/src/main/java/com/google/firebase/samples/apps/mlkit/ui/gallery/GalleryViewModel.java
@@ -4,13 +4,27 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+
 public class GalleryViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private ArrayList<String> listOfSubjects;
+
+    public ArrayList<String> getListOfSubjects() {
+        return listOfSubjects;
+    }
 
     public GalleryViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("Defaulters List");
+        listOfSubjects = new ArrayList<>();
+        listOfSubjects.add("CE TOC (TE3)");
+        listOfSubjects.add("CE TOC (TE2)");
+        listOfSubjects.add("CE SEPM (TE3)");
+        listOfSubjects.add("CE DSL (SE4)");
+        listOfSubjects.add("CE SDL (TE1)");
+        listOfSubjects.add("CE SDL (TE2)");
     }
 
     public LiveData<String> getText() {
