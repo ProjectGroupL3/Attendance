5008712a48b40b814830afcd69c35b9286e6b7f5
Ashawe <harshsaglani@gmail.com>
2019-09-26 09:18:48 +0530

    Implement Login UI

---
+++ app/src/main/java/com/google/firebase/samples/apps/mlkit/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.google.firebase.samples.apps.mlkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
