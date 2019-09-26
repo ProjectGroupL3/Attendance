423c9ff7d351bad4f8e27a497fddb21a29301dca
Ashawe <harshsaglani@gmail.com>
2019-09-26 09:30:23 +0530

    Implement OTP UI

---
+++ app/src/main/java/com/google/firebase/samples/apps/mlkit/OTPActivity.java
@@ -0,0 +1,14 @@
+package com.google.firebase.samples.apps.mlkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+    }
+}
